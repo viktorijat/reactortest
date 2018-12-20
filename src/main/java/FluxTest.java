2d371255e8eda48cb8a366234b5f00d3ea4b5974
Viktorija Tosheva <viktorija.tosheva@tipico.com>
2018-12-20 16:38:32 +0100

    start

--- src/main/java/FluxTest.java
+++ src/main/java/FluxTest.java
@@ -1,2 +1,30 @@
+import java.time.Duration;
+import java.util.Arrays;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
 public class FluxTest {
+
+    @Test
+    public void firstFlux() {
+        Flux.just("A", "B", "C").log().subscribe();
+    }
+
+    @Test
+    public void fluxFromIterable() {
+        Flux.fromIterable(Arrays.asList("A", "B", "C")).log().subscribe();
+    }
+
+    @Test
+    public void fluxFromRange() {
+        Flux.range(10, 5).log().subscribe();
+    }
+
+    @Test
+    public void fluxFromInterval() {
+        Flux.interval(Duration.ofSeconds(1)).log().subscribe();
+    }
 }
+
+
+//https://blogg.kantega.no/webapp-with-create-react-app-and-spring-boot/
